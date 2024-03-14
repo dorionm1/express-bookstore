21162cd8941b33b0419f59a087a2fb1fc01747db
Dorion Mitchell <dorionmitchell@Dorions-MacBook-Pro.local>
2024-03-14 10:58:42 -0400

    added some validation

---
+++ routes/books.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const Book = require("../models/book");
+const ExpressError = require("../expressError.js")
+const jsonschema = require("jsonschema");
+
+const router = new express.Router();
+
+const isValidUrl = (urlString) => {
+  var urlPattern = new RegExp('^(https?:\\/\\/)?'+
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+
+  '(\\#[-a-z\\d_]*)?$','i');
+return !!urlPattern.test(urlString);
+}
+
+
+/** GET / => {books: [book, ...]}  */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  => {book: book} */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /   bookData => {book: newBook}  */
+
+router.post("/", async function (req, res, next) {
+  try {
+    if (!req.body.author || req.body.author === "") {
+      throw new ExpressError("Author is required", 400);
+    }
+    if(!isValidUrl(req.body.amazon_url)) {
+      throw new ExpressError("Invalid URL!", 400);
+    }
+    const book = await Book.create(req.body)
+    return res.status(201).json({ book });
+  } catch (err) {
+    return next(err); // Pass the error to the error-handling middleware
+  }
+});
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.update(req.params.isbn, req.body);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
